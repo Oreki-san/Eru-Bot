9874c2a4c704851d1be267f7864fd86917c0f8b2
RedBeeツ <77657831+bilal-08@users.noreply.github.com>
2021-08-30 22:28:05 +0530

    Revert "Add files via upload"
    This reverts commit 81f40f94a55c1236440a1daa10290be34e397d40.

--- grouplinks.js
+++
@@ -1,9 +0,0 @@
-let handler = async m => m.reply('your message', m.sender)
-
-
-handler.help = ['help 1']
-handler.tags = ['info']
-handler.command = /^support$/i
-// handler.customPrefix = /^.*help ?1$/i
-
-module.exports = handler
