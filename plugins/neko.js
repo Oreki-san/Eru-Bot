3638be3de2abd143983dd6c83e9141493043fc52
Oreki-san <77143046+Oreki-san@users.noreply.github.com>
2021-09-03 14:18:12 +0530

    Update neko.js

--- plugins/neko.js
+++ plugins/neko.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch')
 let handler = async(m, { conn }) => {
-  let res = await fetch('https://yuabot.com/api/img/slap')
+  let res = await fetch('http://api.waifu.pics/sfw/neko')
   if (!res.ok) throw await res.text()
   let json = await res.json()
   if (!json.url) throw 'Error!'
