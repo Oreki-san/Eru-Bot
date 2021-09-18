ba983d2ac698e448418931bd8a103988e57fdb28
Oreki-san <77143046+Oreki-san@users.noreply.github.com>
2021-09-18 18:44:15 +0530

    Update handler.js

--- handler.js
+++ handler.js
@@ -356,14 +356,14 @@ Untuk mematikan fitur ini, ketik
 
 global.dfail = (type, m, conn) => {
   let msg = {
-    rowner: 'Perintah ini hanya dapat digunakan oleh _*OWWNER!1!1!*_',
-    owner: 'Perintah ini hanya dapat digunakan oleh _*Owner Bot*_!',
-    mods: 'Perintah ini hanya dapat digunakan oleh _*Moderator*_ !',
-    premium: 'Perintah ini hanya untuk member _*Premium*_ !',
-    group: 'Perintah ini hanya dapat digunakan di grup!',
-    private: 'Perintah ini hanya dapat digunakan di Chat Pribadi!',
-    admin: 'Perintah ini hanya untuk *Admin* grup!',
-    botAdmin: 'Jadikan bot sebagai *Admin* untuk menggunakan perintah ini!',
+    rowner: '*This command can be only used by* _*OWWNER!1!1!*_',
+    owner: '*This command can be only used by* _*owner Bot*_!',
+    mods: '*This command can be only used by* _*Moderator*_ !',
+    premium: '*This command can be used by members* _*Premium*_ !',
+    group: '*This command can only be used in groups*!',
+    private: '*This command can be only used in private chats!',
+    admin: 'This command can be used by *Admin* group!',
+    botAdmin: 'Make bot admin to use this command!',
     unreg: 'Silahkan daftar untuk menggunakan fitur ini dengan cara mengetik:\n\n*#daftar nama.umur*\n\nContoh: *#daftar Manusia.16*'
   }[type]
   if (msg) return m.reply(msg)
