1a1b71eddd990b56fd3e9defb6dd084177a5ad22
Oreki-san <77143046+Oreki-san@users.noreply.github.com>
2021-09-21 17:13:35 +0530

    Update _tictactoe.js

--- plugins/_tictactoe.js
+++ plugins/_tictactoe.js
@@ -57,11 +57,11 @@ handler.before = function (m) {
 ${arr.slice(0, 3).join('')}
 ${arr.slice(3, 6).join('')}
 ${arr.slice(6).join('')}
-${isWin ? `@${winner.split('@')[0]} Menang! (+${winScore} XP)` : isTie ? `Game berakhir (+${playScore} XP)` : `Giliran ${['❌', '⭕'][1 * room.game._currentTurn]} (@${room.game.currentTurn.split('@')[0]})`}
+${isWin ? `@${winner.split('@')[0]} Won! (+${winScore} XP)` : isTie ? `Game berakhir (+${playScore} XP)` : `Turn ${['❌', '⭕'][1 * room.game._currentTurn]} (@${room.game.currentTurn.split('@')[0]})`}
 
 ❌: @${room.game.playerX.split('@')[0]}
 ⭕: @${room.game.playerO.split('@')[0]}
-Ketik *nyerah* untuk nyerah
+When giving up to give up.
 Room ID: ${room.id}
 `.trim()
         let users = global.DATABASE._data.users
