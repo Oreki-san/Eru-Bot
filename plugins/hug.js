24f9bc3edf754856e295ac3f615dae274ebbdbee
Oreki-san <77143046+Oreki-san@users.noreply.github.com>
2021-09-23 15:56:01 +0530

    Update hug.js

--- plugins/hug.js
+++ plugins/hug.js
@@ -1,11 +1,10 @@
-
 let fetch = require('node-fetch')
 let handler = async(m, { conn }) => {
   let res = await fetch('https://api.waifu.pics/sfw/hug')
   if (!res.ok) throw await res.text()
   let json = await res.json()
   if (!json.url) throw 'Error!'
-  conn.sendFile(m.chat, json.url, '',  {contextinfo : `@${m.sender.split(`@`)[0]} hugged @${m.mentionedJid[0].split(`@`)[0]} `, {mentionedJid : [m.sender , m.mentionedJid[0]]}
+  conn.sendFile(m.chat, json.url, '',  {caption : `@${m.sender.split(`@`)[0]} hugged @${m.mentionedJid[0].split(`@`)[0]} `, contextInfo : {mentionedJid : [m.sender , m.mentionedJid[0]]}
 })
 handler.help = ['neko']
 handler.tags = ['internet']
