3e7726c8af3a37490fdaff839e27633650a77315
Oreki-san <77143046+Oreki-san@users.noreply.github.com>
2021-09-25 22:25:55 +0530

    Add files via upload

--- plugins/cuddle.js
+++ plugins/cuddle.js
@@ -3,18 +3,18 @@ let handler = async(m, { conn }) => {
 
     if(m.quoted?.sender) m.mentionedJid.push(m.quoted.sender)
     if(!m.mentionedJid.length) m.mentionedJid.push(m.sender)
-  let res = await fetch('https://api.waifu.pics/sfw/kick')
+  let res = await fetch('https://api.waifu.pics/sfw/cuddle')
   if (!res.ok) throw await res.text()
   let json = await res.json()
   if (!json.url) throw 'Error!'
 
-  conn.sendFile(m.chat,json.url,'h.gif',`@${m.sender.split('@')[0]} kicked ${m.mentionedJid.map((user)=>(user === m.sender)? 'themselves ': `@${user.split('@')[0]}`).join(', ')}`,m,false,
+  conn.sendFile(m.chat,json.url,'h.gif',`@${m.sender.split('@')[0]} cuddled ${m.mentionedJid.map((user)=>(user === m.sender)? 'themselves ': `@${user.split('@')[0]}`).join(', ')}`,m,false,
   {  contextInfo :{mentionedJid : [  ...m.mentionedJid,m.sender ] }})
   
 
 }
 handler.help = ['neko']
 handler.tags = ['internet']
-handler.command = /^kick$/i
+handler.command = /^cuddle$/i
 
 module.exports = handler
