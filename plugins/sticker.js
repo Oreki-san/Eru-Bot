071a2cf555e48defaf291f091077a52a641fe82c
Oreki-san <77143046+Oreki-san@users.noreply.github.com>
2021-10-08 12:37:55 +0530

    Update sticker.js

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -1,33 +1,33 @@
 const { MessageType } = require('@adiwajshing/baileys')
 const { sticker } = require('../lib/sticker')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-  var stiker = false
-  
+  let stiker = false
+  try {
     let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || ''
     if (/image/.test(mime)) {
       let img = await q.download()
-      if (!img) throw `reply to image with caption ${usedPrefix + command}`
-   var   stiker = await sticker(img, false, 'Chitanda Eru', 'Oreki-San')
+      if (!img) throw `reply to image with caption *${usedPrefix + command}*`
+      stiker = await sticker(img, false, global.packname, global.author)
     } else if (/video/.test(mime)) {
       if ((q.msg || q).seconds > 11) return m.reply('10 seconds max!')
       let img = await q.download()
-      if (!img) throw `reply video/gif with caption ${usedPrefix + command}`
-    var  stiker = await sticker(img, false, 'Chitanda Eru', 'Oreki-San')
+      if (!img) throw `reply video/gif with caption *${usedPrefix + command}*`
+      stiker = await sticker(img, false, global.packname, global.author)
     } else if (/webp/.test(mime)) {
       let img = await q.download()
-      if (!img) throw `reply sticker with caption ${usedPrefix + command}`
-  var    stiker = await sticker(img, false, 'Chitanda Eru', 'Oreki-San')
+      if (!img) throw `reply sticker with caption *${usedPrefix + command}*`
+      stiker = await sticker(img, false, global.packname, global.author)
     } else if (args[0]) {
-      if (isUrl(args[0])) stiker = await sticker(false, args[0], 'Chitanda Eru', 'Oreki-San')
+      if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author)
       else return m.reply('Invalid URL!')
     }
-
-    if (stiker){ conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+  } finally {
+    if (stiker) conn.sendMessage(m.chat, stiker, MessageType.sticker, {
       quoted: m
-    })}
+    })
     else throw 'Error, try to reply to the photo/make sure the mime is correct'
-  
+  }
 }
 handler.help = ['stiker (caption|reply media)', 'stiker <url>', 'stikergif (caption|reply media)', 'stikergif <url>']
 handler.tags = ['sticker']
@@ -36,5 +36,5 @@ handler.command = /^s(tic?ker)?(gif)?(wm)?$/i
 module.exports = handler
 
 const isUrl = (text) => {
-  return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%.+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%+.~#?&/=]*)(jpe?g|gif|png)/, 'gi'))
+  return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi'))
 }
